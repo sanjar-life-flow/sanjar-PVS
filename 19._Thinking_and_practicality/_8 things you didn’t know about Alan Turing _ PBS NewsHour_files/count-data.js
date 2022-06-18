880155235313c4d7f2598c8d8b3a6e60a388ea91
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-06-18 23:38:49 +0530

    Shift assets to a folder

--- 19._Thinking_and_practicality/_8 things you didn’t know about Alan Turing _ PBS NewsHour_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"AHPQVZclnr2","comments":0}]});
-}
